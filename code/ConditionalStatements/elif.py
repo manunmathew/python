a3997889d76e27f912d370c4c411873ee4447803
Manu Mathew <manumathew@office-tech-wl-dhcp40.basefarm.no>
2024-07-18 11:31:51 +0200

    :books: conditional statements

---
+++ code/ConditionalStatements/elif.py
@@ -0,0 +1,10 @@
+# Write a program to find the largest of 3 numbers input by the user
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+if (a > b and a > c):
+    print(a, " is largest")
+elif (b > a and b > c):
+    print(b, " is largest")
+else:
+    print(c, " is largest")
---
+++ code/ConditionalStatements/else.py
@@ -0,0 +1,51 @@
+# take alphabet from user and it should print vowel if the user input a,e,i,o,u
+
+a = input("Enter the alphabet: ")
+vowel = ['a', 'e' , 'i', 'o', 'u']
+if (a in vowel):
+   print("Vowel")
+else:
+   print("Not a vowel")
+
+# Write a program to find the largest of 2 number input by the user
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+if (a >b):
+    print(a, " is greater")
+else:
+    print(b, " is greater")
+
+# Write a program to find the smallest of 2 number input by the user
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+if (a < b):
+    print(a, " is smallest")
+else:
+    print(b, " is smallest")
+
+
+# write a program to check a number is odd or even
+
+# even = num%2 == 0
+# odd = num%2 != 0 | num%2==1
+a = int(input("Enter the number: "))
+if (a%2==0):
+    print(a, " is even number")
+else:
+    print(a, " is odd number")
+write a program to check the number is divisible of 5
+
+a = int(input("Enter the number: "))
+if (a%5==0):
+    print(a, " is divisible of 5")
+else:
+    print(a, " is not divisible of 5")
+
+
+Write a program to check the number is divisible by both 2 and 3 else
+
+a = int(input("Enter the number: "))
+if (a%2==0 and a%3==0 ):
+    print(a, " is divisible by both 2 and 3")
+else:
+    print(a, " is not divisible by both 2 and 3")
