cadd4167e62581f33ec6525334060d33b19a1f0b
Manu Mathew <manumathew@Manus-MacBook-OBS.local>
2024-07-19 12:23:51 +0200

    :books: conditional statements

--- code/ConditionalStatements/elif.py
+++ code/ConditionalStatements/elif.py
@@ -8,15 +8,38 @@
 #     print(b, " is largest")
 # else:
 #     print(c, " is largest")
+#
+#
+# # Write a program to find the largest of 3 numbers input by the user
+# a = int(input("Enter the first number: "))
+# b = int(input("Enter the second number: "))
+# c = int(input("Enter the third number: "))
+# if (a > b and a > c):
+#     print(a, " is largest")
+# elif (b > c):
+#     print(b, " is largest")
+# else:
+#     print(c, " is largest")
+
+# input 5 subject mark, total mark , % mark , 90 a, 80 b
 
 
-# Write a program to find the largest of 3 numbers input by the user
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
-c = int(input("Enter the third number: "))
-if (a > b and a > c):
-    print(a, " is largest")
-elif (b > c):
-    print(b, " is largest")
-else:
-    print(c, " is largest")
+# physics = int(input("Enter the marks for Physics : "))
+# chemistry = int(input("Enter the marks for Chemistry : "))
+# maths = int(input("Enter the marks for Maths : "))
+# english = int(input("Enter the marks for English : "))
+# computer = int(input("Enter the marks for Computer : "))
+# total = physics+chemistry+maths+english+computer
+# percentage = (total/500)*100
+# print("Total Marks =", total)
+# print("Percentage =", percentage)
+# if (percentage >= 90):
+#     print("A Grade")
+# elif (percentage >= 80):
+#     print("B Grade")
+# elif (percentage >= 70):
+#     print("C Grade")
+# elif (percentage >= 60):
+#     print("D Grade")
+# else:
+#     print("Failed")
