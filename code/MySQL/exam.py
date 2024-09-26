bc93746542094934b8d255334001eb0be252a2d2
Manu Mathew <manumathew@Manus-MacBook-OBS.local>
2024-09-26 13:54:34 +0200

    :art: sql

---
+++ code/MySQL/exam.py
@@ -0,0 +1,12 @@
+# text = 'AJhohhjjvhgvFHJ%ML2598gthn#&*@hd'
+
+# CapCount = 0
+# SpCount = 0
+
+# for i in text:
+#     if i.isalpha() and i.isupper():
+#         CapCount += 1
+#     elif not i.isalnum() :
+#         SpCount += 1
+# print(CapCount)
+# print(SpCount)
---
+++ code/SQL/test.py
@@ -0,0 +1,12 @@
+# create a database products
+# create 2 tables
+# product_details .. id, product_id, product_name, price, description
+# customer_details .. id, customer_name, address, phone
+
+# insert 5 data for each
+# select product details
+# select customer details
+# select product_name and price of a product using product id
+# select customer details using phone number
+# select max price product details
+# select customer details in ascending order using name 
