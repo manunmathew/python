b9a1a0ed22e1703c9163bce3e48f7cafae2e5e36
Manu Mathew <manumathew@Manus-MacBook-OBS.local>
2024-11-21 08:16:53 +0100

    update Django

--- code/Projects/ExpenseManager/FundTracker/FundTracker/urls.py
+++ code/Projects/ExpenseManager/FundTracker/FundTracker/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from expense.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('expence/add', ExpenseCreateView.as_view())
 ]
